ff34d9f83addc34334bf6715a0e2d433b50ad74e
Owen Winter <owenwinter1@gmail.com>
2022-01-20 18:00:07 +0000

    Color scales

--- R/ow_cols.R
+++ R/ow_cols.R
@@ -68,3 +68,39 @@ ow_pal <- function(palette = "main", reverse = FALSE, ...) {
   colorRampPalette(pal, ...)
 }
 
+#' Color scale constructor for ow colors
+#'
+#' @param palette Character name of palette in ow_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_color_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_color_ow <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- ow_pal(palette = palette, reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("colour", paste0("ow_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+#' Fill scale constructor for ow colors
+#'
+#' @param palette Character name of palette in ow_palettes
+#' @param discrete Boolean indicating whether color aesthetic is discrete or not
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to discrete_scale() or
+#'            scale_fill_gradientn(), used respectively when discrete is TRUE or FALSE
+#'
+scale_fill_ow <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- ow_pal(palette = palette, reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("fill", paste0("ow_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
+
